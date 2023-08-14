43828b3f5b29d27d3e9597c4b78345062eaeea86
Teddy Abebe <teddyab729@gmail.com>
2023-08-14 12:14:50 -1100

    Auth

--- server.js
+++ server.js
@@ -27,7 +27,6 @@ app.use("/api/employees", employeeRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
